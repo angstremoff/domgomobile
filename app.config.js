f64f5909613e02863bc272944f2c07704a687018
Angstremoff <Angstremoff@ya.ru>
2025-11-25 18:21:24 +0100

    Поднять версию до 1.0.4 (code 8)

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default ({ config }) => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.anonymous.DomGoMobile',
-      buildNumber: '7'
+      buildNumber: '8'
     },
     android: {
       allowBackup: false,
@@ -37,7 +37,7 @@ export default ({ config }) => {
         backgroundColor: '#ffffff'
       },
       package: 'domgo.rs',
-      versionCode: 7,
+      versionCode: 8,
       config: {
         googleMaps: {
           apiKey: GOOGLE_MAPS_API_KEY
