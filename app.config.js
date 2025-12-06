1a5ff24ec425d06bb9e2543f39ffa6cd02e2cfd3
angstremoff <angstremoff@ya.ru>
2025-12-06 11:19:11 +0100

    Bump version to 1.0.8 and versionCode to 12

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ export default ({ config }) => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.anonymous.DomGoMobile',
-      buildNumber: '11'
+      buildNumber: '12'
     },
     android: {
       allowBackup: false,
@@ -37,7 +37,7 @@ export default ({ config }) => {
         backgroundColor: '#ffffff'
       },
       package: 'domgo.rs',
-      versionCode: 11,
+      versionCode: 12,
       config: {
         googleMaps: {
           apiKey: GOOGLE_MAPS_API_KEY
